ab9157eddc1fb192b3ba7ce6fac80d1bb68add3a
Muhammad Wafa <mq.func@gmail.com>
2025-11-14 01:43:52 +0100

    deploy

--- Dashboard/next.config.ts
+++ Dashboard/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // output: "export", // 👈 this tells Next.js to generate /out automatically
+  output: "export", // 👈 this tells Next.js to generate /out automatically
 };
 
 export default nextConfig;
